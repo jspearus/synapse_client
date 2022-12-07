a5af9ee5fb7bcd6ec4129ae61b6c3c7918a150a7
jspearus <jspearus@gmail.com>
2022-12-07 17:58:46 -0600

    update

--- advent.py
+++ advent.py
@@ -45,6 +45,24 @@ def advent():
         time.sleep(.07)
     return True
 
+def qadvent():
+    day = datetime.date.today()
+    for i in range(50):
+        port.write(str.encode(f"1,{i},0,200,0#"))
+    for i in range(100, -1, -1):
+        port.write(str.encode(f"0,{i},0,100,0#"))
+    day = int(day.strftime("%d")) * 2
+    for i in range(0, (day*2), 2):
+        port.write(str.encode(f"2,{i},100,0,0#"))
+        port.write(str.encode(f"3,{i},100,0,0#"))
+        port.write(str.encode(f"4,{i},100,0,0#"))
+        port.write(str.encode(f"5,{i},100,0,0#"))
+    for i in range(64):
+        port.write(str.encode(f"1,{i+50},100,100,100#"))
+        port.write(str.encode("show1#"))
+        port.write(str.encode("show#"))
+    return True
+
 def morningAdvent():
     day = datetime.date.today()
     for i in range(50):
